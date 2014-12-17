3dfa29f999d491798d3f09dd2a58dde58c95dcdb
Bruce Merry <bmerry@ska.ac.za>
2014-12-17 15:25:41 +0200

    More packaging fixes

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from distutils.core import setup, Extension
+from setuptools import setup, Extension
 import glob
 import numpy
 import sys
@@ -9,6 +9,7 @@ bp_library = 'boost_python-py{0}{1}'.format(sys.version_info.major, sys.version_
 extensions = [
     Extension('_spead2',
         sources=glob.glob('src/common_*.cpp') + glob.glob('src/recv_*.cpp') + glob.glob('src/py_*.cpp'),
+        depends=glob.glob('src/*.h'),
         language='c++',
         include_dirs=['src', numpy.get_include()],
         extra_compile_args=['-std=c++11'],
@@ -16,8 +17,12 @@ extensions = [
 ]
 
 setup(
+    author='Bruce Merry',
+    author_email='bmerry@ska.ac.za',
     name='spead2',
     version='0.1dev0',
     description='High-performance SPEAD decoder',
     ext_package='spead2',
-    ext_modules=extensions)
+    ext_modules=extensions,
+    install_requires=['numpy'],
+    packages=['spead2', 'spead2.recv', 'spead2.test'])
