781c66e07c0d8669587598de33d0beb7a4b2c1c6
Bruce Merry <bmerry@ska.ac.za>
2014-12-18 12:05:11 +0200

    Increase ringbuffer size to avoid dropping heaps

--- tests/test.py
+++ tests/test.py
@@ -8,7 +8,7 @@ logging.basicConfig(level=logging.INFO)
 
 items = []
 
-stream = spead2.recv.Stream(spead2.BUG_COMPAT_PYSPEAD_0_5_2, 2)
+stream = spead2.recv.Stream(spead2.BUG_COMPAT_PYSPEAD_0_5_2, 8)
 receiver = spead2.recv.Receiver()
 if 0:
     with open('junkspeadfile', 'rb') as f:
@@ -20,6 +20,7 @@ receiver.start()
 
 ig = spead2.recv.ItemGroup()
 for heap in stream:
+    print "Got heap", heap.cnt
     ig.update(heap)
     for item in ig.items.itervalues():
         print heap.cnt, item.name, item.value.shape
