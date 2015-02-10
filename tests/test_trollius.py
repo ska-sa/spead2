d3f26d7d50add83cc973f374a11716a1e2e7665e
Bruce Merry <bmerry@ska.ac.za>
2015-02-10 23:00:49 +0200

    Update test_trollius to match API changes

--- tests/test_trollius.py
+++ tests/test_trollius.py
@@ -23,14 +23,13 @@ logging.basicConfig(level=logging.DEBUG)
 
 items = []
 
-receiver = spead2.recv.Receiver()
+thread_pool = spead2.ThreadPool(4)
 coros = []
 pool = spead2.Mempool(16384, 26214400, 32, 16)
 for i in range(4):
-    stream = spead2.recv.trollius.Stream(spead2.BUG_COMPAT_PYSPEAD_0_5_2)
+    stream = spead2.recv.trollius.Stream(thread_pool, spead2.BUG_COMPAT_PYSPEAD_0_5_2)
     stream.set_mempool(pool)
-    receiver.add_udp_reader(stream, 8888 + i)
+    stream.add_udp_reader(8888 + i)
     coros.append(run(stream))
-receiver.start(4)
 
 trollius.get_event_loop().run_until_complete(trollius.wait(coros))
