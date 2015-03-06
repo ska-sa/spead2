94987ed3331c6531c4834f6dceb8d9bf0dbe211f
Bruce Merry <bmerry@ska.ac.za>
2015-03-06 20:06:34 +0200

    Make test_send send larger heaps

--- tests/test_send.py
+++ tests/test_send.py
@@ -12,8 +12,9 @@ stream = spead2.send.UdpStream(thread_pool,
     "localhost", 8888, 48, spead2.BUG_COMPAT_PYSPEAD_0_5_2, 1500, 1e7)
 del thread_pool
 
-item = spead2.Item(0x1234, 'foo', 'a foo item', shape=(4, 5), dtype=np.int32)
-item.value = np.zeros([4, 5], np.int32)
+shape = (40, 50)
+item = spead2.Item(0x1234, 'foo', 'a foo item', shape=shape, dtype=np.int32)
+item.value = np.zeros(shape, np.int32)
 heap = spead2.send.Heap(1, spead2.BUG_COMPAT_PYSPEAD_0_5_2)
 heap.add_descriptor(item)
 heap.add_item(item.id, item)
