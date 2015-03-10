bd467004bda0338c8aeb0d94e088558bd3cd5f2c
Bruce Merry <bmerry@ska.ac.za>
2015-03-10 11:09:10 +0200

    Add streambuf_stream (not used for anything yet)

---
+++ src/send_streambuf.cpp
@@ -0,0 +1,28 @@
+/**
+ * @file
+ */
+
+#include <streambuf>
+#include "send_streambuf.h"
+
+namespace spead
+{
+namespace send
+{
+
+streambuf_stream::streambuf_stream(
+    boost::asio::io_service &io_service,
+    std::streambuf &streambuf,
+    int heap_address_bits,
+    bug_compat_mask bug_compat,
+    std::size_t max_packet_size,
+    double rate,
+    std::size_t max_heaps)
+    : stream<streambuf_stream>(
+        io_service, heap_address_bits, bug_compat, max_packet_size, rate, max_heaps),
+        streambuf(streambuf)
+{
+}
+
+} // namespace send
+} // namespace spead
---
+++ src/send_streambuf.h
@@ -0,0 +1,46 @@
+/**
+ * @file
+ */
+
+#ifndef SPEAD_SEND_STREAMBUF_H
+#define SPEAD_SEND_STREAMBUF_H
+
+#include <streambuf>
+#include <boost/asio.hpp>
+#include "send_stream.h"
+
+namespace spead
+{
+namespace send
+{
+
+class streambuf_stream : public stream<streambuf_stream>
+{
+private:
+    friend class stream<streambuf_stream>;
+    std::streambuf &streambuf;
+
+    template<typename Handler>
+    void async_send_packet(const packet &pkt, Handler &&handler)
+    {
+        for (const auto &buffer : pkt.buffers)
+            streambuf.sputn(boost::asio::buffer_cast<const char *>(buffer),
+                            boost::asio::buffer_size(buffer));
+        get_io_service().dispatch(std::move(handler));
+    }
+
+public:
+    streambuf_stream(
+        boost::asio::io_service &io_service,
+        std::streambuf &streambuf,
+        int heap_address_bits,
+        bug_compat_mask bug_compat,
+        std::size_t max_packet_size,
+        double rate = 0.0,
+        std::size_t max_heaps = default_max_heaps);
+};
+
+} // namespace send
+} // namespace spead
+
+#endif // SPEAD_SEND_STREAMBUF_H
