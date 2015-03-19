fd3a1290fa01a9a880a57cf30f06fffae9422209
Bruce Merry <bmerry@ska.ac.za>
2015-03-19 13:28:13 -0700

    Make streambuf code compile (wrong binding for handler)

--- src/send_streambuf.h
+++ src/send_streambuf.h
@@ -6,6 +6,7 @@
 #define SPEAD_SEND_STREAMBUF_H
 
 #include <streambuf>
+#include <functional>
 #include <boost/asio.hpp>
 #include "send_stream.h"
 
@@ -23,10 +24,15 @@ private:
     template<typename Handler>
     void async_send_packet(const packet &pkt, Handler &&handler)
     {
+        std::size_t size = 0;
         for (const auto &buffer : pkt.buffers)
-            streambuf.sputn(boost::asio::buffer_cast<const char *>(buffer),
-                            boost::asio::buffer_size(buffer));
-        get_io_service().dispatch(std::move(handler));
+        {
+            std::size_t buffer_size = boost::asio::buffer_size(buffer);
+            // TODO: handle errors
+            streambuf.sputn(boost::asio::buffer_cast<const char *>(buffer), buffer_size);
+            size += buffer_size;
+        }
+        get_io_service().dispatch(std::bind(std::move(handler), boost::system::error_code(), size));
     }
 
 public:
