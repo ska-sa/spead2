357adc722729ea676928162c90da73825deb5c5d
Bruce Merry <bmerry@ska.ac.za>
2015-06-06 12:42:35 +0200

    Prepare for posting docs on readthedocs.org

--- doc/conf.py
+++ doc/conf.py
@@ -14,11 +14,17 @@
 
 import sys
 import os
+import os.path
+import subprocess
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('..'))
+
+# Build doxygen first on readthedocs
+if os.environ.get('READTHEDOCS'):
+    subprocess.check_call(['doxygen'])
 
 # -- General configuration ------------------------------------------------
 
