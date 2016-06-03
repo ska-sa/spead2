ce5c829263ae5bc735d001461eb6c279322f4277
Bruce Merry <bmerry@ska.ac.za>
2016-06-03 14:39:44 +0200

    Add slightly better validation of memory allocators

--- src/unittest_memory_allocator.cpp
+++ src/unittest_memory_allocator.cpp
@@ -18,6 +18,9 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(allocator_test, T, test_types)
     typedef typename T::pointer pointer;
     std::shared_ptr<T> allocator = std::make_shared<T>();
     pointer ptr = allocator->allocate(12345, nullptr);
+    // Scribble on the memory to make sure it is really allocated
+    for (std::size_t i = 0; i < 12345; i++)
+        ptr[i] = 1;
     ptr.reset();
 }
 
