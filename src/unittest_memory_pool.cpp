f824a7e7a0f2028d29fc324984827de069f55197
Bruce Merry <bmerry@ska.ac.za>
2016-06-03 14:01:04 +0200

    Add another unit test for memory pools

--- src/unittest_memory_pool.cpp
+++ src/unittest_memory_pool.cpp
@@ -24,6 +24,84 @@ BOOST_AUTO_TEST_CASE(memory_pool_refill)
         pointers.push_back(pool->allocate(1024 * 1024));
 }
 
+class mock_allocator : public spead2::memory_allocator
+{
+public:
+    struct record
+    {
+        bool allocate;
+        std::size_t size;
+        void *ptr;
+    };
+
+    std::vector<record> records;
+    std::vector<std::unique_ptr<std::uint8_t[]>> saved;
+
+    virtual pointer allocate(std::size_t size) override
+    {
+        std::uint8_t *ptr = new std::uint8_t[size];
+        records.push_back(record{true, size, ptr});
+        return pointer(ptr, deleter(shared_from_this(), ptr - 1));
+    }
+
+private:
+    virtual void free(std::uint8_t *ptr, void *user) override
+    {
+        BOOST_CHECK_EQUAL(ptr - 1, user);
+        records.push_back(record{false, 0, ptr});
+        saved.emplace_back(ptr);
+    }
+};
+
+// Check that the user pointer is passed through to the underlying allocator,
+// and generally interacts with the underlying allocator correctly
+BOOST_AUTO_TEST_CASE(memory_pool_pass_user)
+{
+    typedef spead2::memory_allocator::pointer pointer;
+    std::shared_ptr<mock_allocator> allocator = std::make_shared<mock_allocator>();
+    std::shared_ptr<spead2::memory_pool> pool = std::make_shared<spead2::memory_pool>(
+        1024, 2048, 2, 1, allocator);
+
+    // Pooled allocation, comes from the pre-allocated slot
+    pointer p1 = pool->allocate(1536);
+    // Pooled allocations, newly allocated
+    pointer p2 = pool->allocate(1500);
+    pointer p3 = pool->allocate(1024);
+
+    // Free all the pointers. p3 should be dropped
+    p1.reset(); p2.reset(); p3.reset();
+
+    // Make another allocation, which should come from the pool (p1)
+    pointer p4 = pool->allocate(1600);
+    // Allocation not from the pool
+    pointer p5 = pool->allocate(2049);
+
+    // Release our reference to the pool. It should be destroyed once p4 is freed
+    pool.reset();
+    // Free remaining pointers
+    p4.reset();
+    p5.reset();
+
+    // Check results
+    BOOST_REQUIRE_EQUAL(allocator->records.size(), 8);
+    BOOST_CHECK_EQUAL(allocator->records[0].allocate, true);
+    BOOST_CHECK_EQUAL(allocator->records[0].size, 2048);
+    BOOST_CHECK_EQUAL(allocator->records[1].allocate, true);
+    BOOST_CHECK_EQUAL(allocator->records[1].size, 2048);
+    BOOST_CHECK_EQUAL(allocator->records[2].allocate, true);
+    BOOST_CHECK_EQUAL(allocator->records[2].size, 2048);
+    BOOST_CHECK_EQUAL(allocator->records[3].allocate, false);
+    BOOST_CHECK_EQUAL(allocator->records[3].ptr, allocator->records[2].ptr);
+    BOOST_CHECK_EQUAL(allocator->records[4].allocate, true);
+    BOOST_CHECK_EQUAL(allocator->records[4].size, 2049);
+    BOOST_CHECK_EQUAL(allocator->records[5].allocate, false);
+    BOOST_CHECK_EQUAL(allocator->records[5].ptr, allocator->records[0].ptr);
+    BOOST_CHECK_EQUAL(allocator->records[6].allocate, false);
+    BOOST_CHECK_EQUAL(allocator->records[6].ptr, allocator->records[1].ptr);
+    BOOST_CHECK_EQUAL(allocator->records[7].allocate, false);
+    BOOST_CHECK_EQUAL(allocator->records[7].ptr, allocator->records[4].ptr);
+}
+
 BOOST_AUTO_TEST_SUITE_END()  // memory_pool
 BOOST_AUTO_TEST_SUITE_END()  // common
 
