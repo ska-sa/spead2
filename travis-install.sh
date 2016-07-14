6fd722813192d6c6c64bb8586b64cb17cb8912a0
Bruce Merry <bmerry@ska.ac.za>
2016-07-14 10:06:34 +0200

    Add OS X tests to Travis

--- travis-install.sh
+++ travis-install.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 set -e -x
-if [ "$TEST" = "python2" ]; then
-    sudo -H pip install -U pip setuptools wheel
-    sudo -H pip install -r requirements.txt
-    sudo -H pip install "git+https://github.com/ska-sa/PySPEAD#egg=spead"
+
+if [[ "$TEST" == "python3" ]]; then
+    PIP=pip3
+else
+    PIP=pip
+fi
+if [[ "$TRAVIS_OS_NAME" != "osx" ]]; then
+    PIP="sudo -H $PIP"
 fi
 
-if [ "$TEST" = "python3" ]; then
-    sudo -H pip3 install -U pip setuptools wheel
-    sudo -H pip3 install -r requirements.txt
+if [[ "$TEST" == "python2" || "$TEST" == "python3" ]]; then
+    $PIP install -U pip setuptools wheel
+    $PIP install -r requirements.txt
+    if [[ "$TEST" == "python2" ]]; then
+        $PIP install "git+https://github.com/ska-sa/PySPEAD#egg=spead"
+    fi
 fi
 
-if [ "$TEST" = "cxx" ] && [ "$NETMAP" = "yes" ]; then
+if [ "$NETMAP" = "yes" ]; then
     git clone https://github.com/luigirizzo/netmap
 fi
--- travis-script.sh
+++ travis-script.sh
@@ -1,28 +1,33 @@
 #!/bin/bash
 set -e -x
 
+if [[ "$TEST" == "python3" ]]; then
+    PIP=pip3
+else
+    PIP=pip
+fi
+if [[ "$TRAVIS_OS_NAME" != "osx" ]]; then
+    PIP="sudo -H env CC=$CC $PIP"
+fi
+
 autoreconf --install
 if [ "$TEST" = "cxx" ]; then
     if [ "$NETMAP" = "yes" ]; then
         export CPATH="$PWD/netmap/sys"
     fi
-    AR=ar ./configure \
+    ./configure \
         --with-netmap="$NETMAP" \
         --with-recvmmsg="$RECVMMSG" \
         --with-eventfd="$EVENTFD" \
-        --with-ibv="$IBV"
+        --with-ibv="$IBV" \
+        --disable-optimized
     make -j4
     make -j4 check
 fi
 
-if [ "$TEST" = "python2" ]; then
+if [[ "$TEST" == "python2" || "$TEST" == "python3" ]]; then
     # The -e is necessary to make nosetests work, because otherwise it tries to find the
     # .so in the current directory instead of the install directory.
-    CC="$CC" sudo -H pip install -e .
-    nosetests -v
-fi
-
-if [ "$TEST" = "python3" ]; then
-    CC="$CC" sudo -H pip3 install -e .
+    $PIP install -v -e .
     nosetests -v
 fi
