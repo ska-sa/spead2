9630c67ad02364405fabc1eaac7ddbec7c6486de
Bruce Merry <bmerry@ska.ac.za>
2017-06-18 20:20:09 +0200

    Workaround for portable_endian on old glibc
    The endian.h functions that portable_endian.h nominally exposed don't
    actually exist in glibc before version 2.9. Added another fallback path
    to use the GCC intrinsics. This is only added for the benefit of
    producing manylinux1 wheels, so I haven't bothered trying to support
    clang.

--- include/spead2/portable_endian.h
+++ include/spead2/portable_endian.h
@@ -13,9 +13,49 @@
 
 #endif
 
-#if defined(__linux__) || defined(__CYGWIN__)
+#if defined(__GLIBC__) && (__GLIBC__ > 2 || (__GLIBC__ == 2 && __GLIBC_MINOR__ >= 9))
 
 #	include <endian.h>
+
+#elif defined(__GNUC__) && defined(__BYTE_ORDER__)
+
+#	if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+
+#		define htobe16(x) __builtin_bswap16(x)
+#		define htole16(x) (x)
+#		define be16toh(x) __builtin_bswap16(x)
+#		define le16toh(x) (x)
+ 
+#		define htobe32(x) __builtin_bswap32(x)
+#		define htole32(x) (x)
+#		define be32toh(x) __builtin_bswap32(x)
+#		define le32toh(x) (x)
+ 
+#		define htobe64(x) __builtin_bswap64(x)
+#		define htole64(x) (x)
+#		define be64toh(x) __builtin_bswap64(x)
+#		define le64toh(x) (x)
+
+#	elif __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+
+#		define htobe16(x) (x)
+#		define htole16(x) __builtin_bswap16(x)
+#		define be16toh(x) (x)
+#		define le16toh(x) __builtin_bswap16(x)
+ 
+#		define htobe32(x) (x)
+#		define htole32(x) __builtin_bswap32(x)
+#		define be32toh(x) (x)
+#		define le32toh(x) __builtin_bswap32(x)
+ 
+#		define htobe64(x) (x)
+#		define htole64(x) __builtin_bswap64(x)
+#		define be64toh(x) (x)
+#		define le64toh(x) __builtin_bswap64(x)
+
+#	else
+#		error unknown byte order
+#	endif
 
 #elif defined(__APPLE__)
 
