ska-sa/spead2

commit b891f589ae4fde0a691846f548f0af5195c3ce51

Improve error messages when passing invalid arguments to mcdump
Fixes #50.

scripts/spead2_send.py
@@ -72,7 +72,7 @@ def run(item_group, stream, args):
     if args.heaps is None:
         rep = itertools.repeat(False)
     else:
-        rep = itertools.chain(itertools.repeat(False, args.heaps), [True])
+        rep = itertools.chain(itertools.repeat(False, args.heaps), [])
     for is_end in rep:
         if len(tasks) >= 2:
             yield From(trollius.wait([tasks.popleft()]))

src/mcdump.cpp
@@ -402,6 +402,7 @@ private:
 
 public:
     capture(const options &opts);
+    ~capture();
     void run();
 };
 
@@ -605,10 +606,21 @@ capture::capture(const options &opts)
 {
 }
 
+capture::~capture()
+{
+    // This is needed (in the error case) to unblock the writer threads so that
+    // shutdown doesn't deadlock.
+    w->close();
+}
+
 static boost::asio::ip::udp::endpoint make_endpoint(const std::string &s)
 {
     // Use rfind rather than find because IPv6 addresses contain :'s
     auto pos = s.rfind(':');
+    if (pos == std::string::npos)
+    {
+        throw std::runtime_error("Endpoint " + s + " is missing a port number");
+    }
     try
     {
         boost::asio::ip::address_v4 addr = boost::asio::ip::address_v4::from_string(s.substr(0, pos));
@@ -643,8 +655,15 @@ void capture::run()
     std::vector<udp::endpoint> endpoints;
     for (const std::string &s : opts.endpoints)
         endpoints.push_back(make_endpoint(s));
-    boost::asio::ip::address_v4 interface_address =
-        boost::asio::ip::address_v4::from_string(opts.interface);
+    boost::asio::ip::address_v4 interface_address;
+    try
+    {   
+        interface_address = boost::asio::ip::address_v4::from_string(opts.interface);
+    }
+    catch (std::exception)
+    {
+        throw std::runtime_error("Invalid interface address " + opts.interface);
+    }
 
     std::size_t n_chunks = sizes(opts).second;
     if (std::numeric_limits<std::uint32_t>::max() / max_records <= n_chunks)

commit d4bbf33bf23faebd548baea845ad09934d5cb05f

Fix spead2_send.py to send a stop heap with --heaps
It was acccidentally broken in b891f589 (and 1.2.0 release).

scripts/spead2_send.py
@@ -72,7 +72,7 @@ def run(item_group, stream, args):
     if args.heaps is None:
         rep = itertools.repeat(False)
     else:
-        rep = itertools.chain(itertools.repeat(False, args.heaps), [])
+        rep = itertools.chain(itertools.repeat(False, args.heaps), [True])
     for is_end in rep:
         if len(tasks) >= 2:
             yield From(trollius.wait([tasks.popleft()]))