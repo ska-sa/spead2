2a79ccd560617ec19ebe6144917a8959b416c610
Bruce Merry <bmerry@ska.ac.za>
2017-11-06 11:11:45 +0200

    Fix generate_wheels script to find trollius2asyncio

--- manylinux1/generate_wheels.sh
+++ manylinux1/generate_wheels.sh
@@ -5,7 +5,7 @@ cd /tmp/spead2
 mkdir -p /output
 for d in /opt/python/cp{27,34,35,36}*; do
     git clean -xdf
-    ./bootstrap.sh
+    PATH=$d/bin:$PATH ./bootstrap.sh
     $d/bin/python ./setup.py bdist_wheel -d .
     auditwheel repair -w /output spead2-*-`basename $d`-linux_*.whl
 done
