771414991de50246a1acb0368ddd297982386e7e
Bruce Merry <bmerry@ska.ac.za>
2019-04-30 10:33:43 +0200

    Rename manylinux1 -> manylinux
    In preparation for supporting manylinux2010.

--- mkwheels.sh
+++ mkwheels.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-sudo docker build --pull -t ska-sa/spead2/manylinux1 -f manylinux1/Dockerfile .
+sudo docker build --pull -t ska-sa/spead2/manylinux -f manylinux/Dockerfile .
 mkdir -p wheelhouse
-sudo docker run --rm -v "$PWD/wheelhouse:/wheelhouse" ska-sa/spead2/manylinux1 sh -c 'cp -v /output/*.whl /wheelhouse'
+sudo docker run --rm -v "$PWD/wheelhouse:/wheelhouse" ska-sa/spead2/manylinux sh -c 'cp -v /output/*.whl /wheelhouse'
 sudo chown `id -u`:`id -g` wheelhouse/*.whl
