b02973ecf7f4745806847cfbbaf1f55d4b4e0398
Bruce Merry <bmerry@ska.ac.za>
2019-04-30 13:28:18 +0200

    Build manylinux2010_x86_64 wheels
    These use the MLNX versionn of libibverbs so that it can support all the
    optional features.

--- manylinux/generate_wheels.sh
+++ manylinux/generate_wheels.sh
@@ -7,5 +7,5 @@ for d in /opt/python/cp{27,34,35,36,37}*; do
     git clean -xdf
     PATH=$d/bin:$PATH ./bootstrap.sh
     $d/bin/python ./setup.py bdist_wheel -d .
-    auditwheel repair -w /output spead2-*-`basename $d`-linux_*.whl
+    auditwheel repair --plat manylinux2010_x86_64 -w /output spead2-*-`basename $d`-linux_*.whl
 done
