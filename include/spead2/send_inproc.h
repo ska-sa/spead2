78db6c1253cb639873c497ef1f479df1f3cf7e85
Bruce Merry <bmerry@ska.ac.za>
2019-05-10 17:17:52 +0200

    Update copyright notices

--- include/spead2/send_inproc.h
+++ include/spead2/send_inproc.h
@@ -1,4 +1,4 @@
-/* Copyright 2018 SKA South Africa
+/* Copyright 2018, 2019 SKA South Africa
  *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
