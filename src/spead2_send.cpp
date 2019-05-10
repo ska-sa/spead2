becd1997d8deecf2fc85060228b4e67db20d23ac
Bruce Merry <bmerry@ska.ac.za>
2019-05-10 09:20:39 +0000

    Optimise spead2_send.cpp
    The promise/future-based synchronisation is replaced by doing the work
    in the asio callbacks directly. This has much lower overhead.
    Additionally, the heaps are all pre-constructed. The run function is
    turned into a class to facilitate callbacks that don't need to bind a
    lot of state.
    
    The --threads option is removed because multiple threads are never
    actually useful, and with the current round-robin design can lead to
    heaps getting arbitrarily out-of-order as the threads progress at
    different rates.

--- src/spead2_send.cpp
+++ src/spead2_send.cpp
@@ -1,4 +1,4 @@
-/* Copyright 2016, 2017 SKA South Africa
+/* Copyright 2016, 2017, 2019 SKA South Africa
  *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
@@ -27,6 +27,7 @@
 #include <boost/program_options.hpp>
 #include <boost/asio.hpp>
 #include <spead2/common_thread_pool.h>
+#include <spead2/common_semaphore.h>
 #include <spead2/send_stream.h>
 #include <spead2/send_udp.h>
 #include <spead2/send_tcp.h>
@@ -54,7 +55,6 @@ struct options
     std::size_t burst = spead2::send::stream_config::default_burst_size;
     double burst_rate_ratio = spead2::send::stream_config::default_burst_rate_ratio;
     std::size_t max_heaps = spead2::send::stream_config::default_max_heaps;
-    int threads = 1;
     double rate = 0.0;
     int ttl = 1;
 #if SPEAD2_USE_IBV
@@ -106,7 +106,6 @@ static options parse_args(int argc, const char **argv)
         ("burst", make_opt(opts.burst), "Burst size")
         ("burst-rate-ratio", make_opt(opts.burst_rate_ratio), "Hard rate limit, relative to --rate")
         ("max-heaps", make_opt(opts.max_heaps), "Maximum heaps in flight")
-        ("threads", make_opt(opts.threads), "Number of worker threads")
         ("rate", make_opt(opts.rate), "Transmission rate bound (Gb/s)")
         ("ttl", make_opt(opts.ttl), "TTL for multicast target")
 #if SPEAD2_USE_IBV
@@ -162,37 +161,53 @@ static options parse_args(int argc, const char **argv)
     }
 }
 
-// Sends a heap, returning a future instead of using a completion handler
-std::future<std::size_t> async_send_heap(spead2::send::stream &stream, const spead2::send::heap &heap)
+class sender
 {
-    auto promise = std::make_shared<std::promise<std::size_t>>();
-    auto handler = [promise] (boost::system::error_code ec, std::size_t bytes_transferred)
-    {
-        if (ec)
-            promise->set_exception(std::make_exception_ptr(boost::system::system_error(ec)));
-        else
-            promise->set_value(bytes_transferred);
-    };
-    stream.async_send_heap(heap, handler);
-    return promise->get_future();
-}
+private:
+    spead2::send::stream &stream;
+    const std::size_t max_heaps;
+    const std::int64_t n_heaps;
+    const spead2::flavour flavour;
 
-int run(spead2::send::stream &stream, const options &opts)
-{
+    spead2::send::heap first_heap;                           // has descriptors
+    std::vector<spead2::send::heap> heaps;
+    spead2::send::heap last_heap;                            // has end-of-stream marker
     typedef std::pair<float, float> item_t;
-    std::size_t elements = opts.heap_size / (opts.items * sizeof(item_t));
-    std::size_t heap_size = elements * opts.items * sizeof(item_t);
+    std::vector<std::unique_ptr<item_t[]>> values;
+
+    std::uint64_t bytes_transferred = 0;
+    boost::system::error_code error;
+    spead2::semaphore done_sem{0};
+
+    const spead2::send::heap &get_heap(std::uint64_t idx) const noexcept;
+
+    void callback(std::uint64_t idx, const boost::system::error_code &ec, std::size_t bytes_transferred);
+
+public:
+    sender(spead2::send::stream &stream, const options &opts);
+    std::uint64_t run();
+};
+
+sender::sender(spead2::send::stream &stream, const options &opts)
+    : stream(stream),
+    max_heaps(opts.max_heaps),
+    n_heaps((opts.heaps < 0 || std::uint64_t(opts.heaps) >= max_heaps) ? opts.heaps : max_heaps),
+    flavour(spead2::maximum_version, 64, opts.addr_bits,
+            opts.pyspead ? spead2::BUG_COMPAT_PYSPEAD_0_5_2 : 0),
+    first_heap(flavour),
+    last_heap(flavour)
+{
+    heaps.reserve(max_heaps);
+    for (std::size_t i = 0; i < max_heaps; i++)
+        heaps.emplace_back(flavour);
+
+    const std::size_t elements = opts.heap_size / (opts.items * sizeof(item_t));
+    const std::size_t heap_size = elements * opts.items * sizeof(item_t);
     if (heap_size != opts.heap_size)
     {
         std::cerr << "Heap size is not an exact multiple: using " << heap_size << " instead of " << opts.heap_size << '\n';
     }
 
-    spead2::flavour f(spead2::maximum_version, 64, opts.addr_bits,
-                      opts.pyspead ? spead2::BUG_COMPAT_PYSPEAD_0_5_2 : 0);
-
-    std::vector<spead2::descriptor> descriptors;
-    std::vector<std::unique_ptr<item_t[]>> values;
-    descriptors.reserve(opts.items);
     values.reserve(opts.items);
     for (std::size_t i = 0; i < opts.items; i++)
     {
@@ -205,50 +220,68 @@ int run(spead2::send::stream &stream, const options &opts)
         sstr.str("");
         sstr << "{'shape': (" << elements << ",), 'fortran_order': False, 'descr': '<c8'}";
         d.numpy_header = sstr.str();
-        descriptors.push_back(d);
+        first_heap.add_descriptor(d);
         std::unique_ptr<item_t[]> item(new item_t[elements]);
+        item_t *ptr = item.get();
         values.push_back(std::move(item));
+        for (std::size_t j = 0; j < max_heaps; j++)
+            heaps[j].add_item(0x1000 + i, ptr, elements * sizeof(item_t), true);
+        first_heap.add_item(0x1000 + i, ptr, elements * sizeof(item_t), true);
+    }
+    last_heap.add_end();
+}
+
+const spead2::send::heap &sender::get_heap(std::uint64_t idx) const noexcept
+{
+    if (idx == 0)
+        return first_heap;
+    else if (n_heaps >= 0 && idx == std::uint64_t(n_heaps))
+        return last_heap;
+    else
+        return heaps[idx % max_heaps];
+}
+
+void sender::callback(std::uint64_t idx, const boost::system::error_code &ec, std::size_t bytes_transferred)
+{
+    this->bytes_transferred += bytes_transferred;
+    if (ec && !error)
+        error = ec;
+    if (error)
+    {
+        done_sem.put();
+        return;
     }
 
-    std::deque<std::future<std::size_t>> futures;
-    std::deque<spead2::send::heap> heaps;
-    std::uint64_t sent = 0;
-    std::uint64_t sent_bytes = 0;
+    if (n_heaps == -1 || std::uint64_t(n_heaps) - idx >= max_heaps)
+    {
+        idx += max_heaps;
+        stream.async_send_heap(get_heap(idx), [this, idx] (const boost::system::error_code &ec, std::size_t bytes_transferred) {
+            callback(idx, ec, bytes_transferred); });
+    }
+    else
+        done_sem.put();
+}
+
+std::uint64_t sender::run()
+{
+    bytes_transferred = 0;
+    error = boost::system::error_code();
+    for (int i = 0; i < max_heaps; i++)
+        stream.async_send_heap(get_heap(i), [this, i] (const boost::system::error_code &ec, std::size_t bytes_transferred) {
+            callback(i, ec, bytes_transferred); });
+    for (int i = 0; i < max_heaps; i++)
+        semaphore_get(done_sem);
+    if (error)
+        throw boost::system::system_error(error);
+    return bytes_transferred;
+}
+
+int run(spead2::send::stream &stream, const options &opts)
+{
+    sender s(stream, opts);
+
     auto start_time = std::chrono::high_resolution_clock::now();
-    while (opts.heaps < 0 || sent <= std::uint64_t(opts.heaps))
-    {
-        if (futures.size() >= opts.max_heaps)
-        {
-            sent_bytes += futures.front().get();
-            futures.pop_front();
-            heaps.pop_front();
-        }
-
-        heaps.emplace_back(f);
-        spead2::send::heap &heap = heaps.back();
-        if (sent == 0)
-        {
-            for (const auto &d : descriptors)
-                heap.add_descriptor(d);
-        }
-        if (opts.heaps >= 0 && sent == std::uint64_t(opts.heaps))
-        {
-            heap.add_end();
-        }
-        else
-        {
-            for (std::size_t i = 0; i < opts.items; i++)
-                heap.add_item(0x1000 + i, values[i].get(), elements * sizeof(item_t), true);
-        }
-        futures.push_back(async_send_heap(stream, heap));
-        sent++;
-    }
-    while (futures.size() > 0)
-    {
-        sent_bytes += futures.front().get();
-        futures.pop_front();
-        heaps.pop_front();
-    }
+    std::uint64_t sent_bytes = s.run();
     auto stop_time = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> elapsed = stop_time - start_time;
     double elapsed_s = elapsed.count();
@@ -271,7 +304,7 @@ int main(int argc, const char **argv)
 {
     options opts = parse_args(argc, argv);
 
-    spead2::thread_pool thread_pool(opts.threads);
+    spead2::thread_pool thread_pool(1);
     spead2::send::stream_config config(
         opts.packet, opts.rate * 1000 * 1000 * 1000 / 8, opts.burst,
         opts.max_heaps, opts.burst_rate_ratio);
@@ -284,7 +317,7 @@ int main(int argc, const char **argv)
     if (opts.tcp) {
         tcp::endpoint endpoint = get_endpoint<tcp>(io_service, opts);
         auto promise = std::promise<void>();
-        auto connect_handler = [&promise] (boost::system::error_code e) {
+        auto connect_handler = [&promise] (const boost::system::error_code &e) {
             if (e)
                 promise.set_exception(std::make_exception_ptr(boost::system::system_error(e)));
             else
