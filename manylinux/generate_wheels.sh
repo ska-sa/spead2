06d8d7189c79d606fb5bbffdcf1f9caa2dec6ee6
Bruce Merry <bmerry@ska.ac.za>
2019-10-08 10:34:42 +0200

    generate_wheels: install reqs for bootstrapping
    I have no idea how this ever worked before - presumably they were
    pre-installed for Python 3.5-3.7 but not 3.8.

--- manylinux/generate_wheels.sh
+++ manylinux/generate_wheels.sh
@@ -5,6 +5,7 @@ cd /tmp/spead2
 mkdir -p /output
 for d in /opt/python/cp{35,36,37,38}*; do
     git clean -xdf
+    $d/bin/pip install jinja2==2.10.1 pycparser==2.19   # For bootstrap
     PATH=$d/bin:$PATH ./bootstrap.sh
     $d/bin/python ./setup.py bdist_wheel -d .
     auditwheel repair --plat manylinux2010_x86_64 -w /output spead2-*-`basename $d`-linux_*.whl
