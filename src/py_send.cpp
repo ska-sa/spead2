e0e336602d7754ae88a6d48df2401523a0661195
Bruce Merry <bmerry@ska.ac.za>
2020-09-04 14:49:09 +0200

    Fix py_send wrapper
    It wasn't compiling because I forgot to add a parameter

--- src/py_send.cpp
+++ src/py_send.cpp
@@ -684,7 +684,7 @@ py::module register_module(py::module &parent)
         .def("__next__", &packet_generator_next);
 
     py::class_<stream_config>(m, "StreamConfig")
-        .def(py::init<std::size_t, double, std::size_t, std::size_t, double>(),
+        .def(py::init<std::size_t, double, std::size_t, std::size_t, double, bool>(),
              "max_packet_size"_a = stream_config::default_max_packet_size,
              "rate"_a = 0.0,
              "burst_size"_a = stream_config::default_burst_size,
