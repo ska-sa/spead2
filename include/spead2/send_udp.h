7fd680e2ebe252956c0c918d73c089b4c8bd5cb9
Bruce Merry <bmerry@ska.ac.za>
2020-09-10 15:50:32 +0200

    Bump more copyright years

--- include/spead2/send_udp.h
+++ include/spead2/send_udp.h
@@ -1,4 +1,4 @@
-/* Copyright 2015, 2019 SKA South Africa
+/* Copyright 2015, 2019-2020 SKA South Africa
  *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
