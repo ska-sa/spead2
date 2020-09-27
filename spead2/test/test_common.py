cd896af431da6ccf71d7e3b460a63816f5e8f557
Bruce Merry <bmerry@ska.ac.za>
2020-09-27 11:11:04 +0200

    Convert test_common.py to pytest

--- spead2/test/test_common.py
+++ spead2/test/test_common.py
@@ -1,4 +1,4 @@
-# Copyright 2015, 2019 SKA South Africa
+# Copyright 2015, 2019-2020 SKA South Africa
 #
 # This program is free software: you can redistribute it and/or modify it under
 # the terms of the GNU Lesser General Public License as published by the Free
@@ -16,23 +16,20 @@
 """Tests for parts of spead2 that are shared between send and receive"""
 
 import numpy as np
-from nose.tools import (
-    assert_equal, assert_greater, assert_raises, assert_true, assert_false,
-    assert_is, assert_is_not, assert_is_none)
+import pytest
 
 import spead2
 
 
 class TestParseRangeList:
     def test_empty(self):
-        assert_equal([], spead2.parse_range_list(''))
+        assert spead2.parse_range_list('') == []
 
     def test_simple(self):
-        assert_equal([1, 2, 5], spead2.parse_range_list('1,2,5'))
+        assert spead2.parse_range_list('1,2,5') == [1, 2, 5]
 
     def test_ranges(self):
-        assert_equal([100, 4, 5, 6, 8, 10, 12, 13],
-                     spead2.parse_range_list('100,4-6,8,10-10,12-13'))
+        assert spead2.parse_range_list('100,4-6,8,10-10,12-13') == [100, 4, 5, 6, 8, 10, 12, 13]
 
 
 class TestThreadPool:
@@ -49,46 +46,46 @@ class TestThreadPool:
         spead2.ThreadPool(1, [1, 0, 2])
 
     def test_zero_threads(self):
-        with assert_raises(ValueError):
+        with pytest.raises(ValueError):
             spead2.ThreadPool(0)
-        with assert_raises(ValueError):
+        with pytest.raises(ValueError):
             spead2.ThreadPool(0, [0, 1])
 
 
 class TestFlavour:
     def test_bad_version(self):
-        with assert_raises(ValueError):
+        with pytest.raises(ValueError):
             spead2.Flavour(3, 64, 40, 0)
 
     def test_bad_item_pointer_bits(self):
-        with assert_raises(ValueError):
+        with pytest.raises(ValueError):
             spead2.Flavour(4, 32, 24, 0)
 
     def test_bad_heap_address_bits(self):
-        with assert_raises(ValueError):
+        with pytest.raises(ValueError):
             spead2.Flavour(4, 64, 43, 0)  # Not multiple of 8
-        with assert_raises(ValueError):
+        with pytest.raises(ValueError):
             spead2.Flavour(4, 64, 0, 0)
-        with assert_raises(ValueError):
+        with pytest.raises(ValueError):
             spead2.Flavour(4, 64, 64, 0)
 
     def test_attributes(self):
         flavour = spead2.Flavour(4, 64, 40, 2)
-        assert_equal(4, flavour.version)
-        assert_equal(64, flavour.item_pointer_bits)
-        assert_equal(40, flavour.heap_address_bits)
-        assert_equal(2, flavour.bug_compat)
+        assert flavour.version == 4
+        assert flavour.item_pointer_bits == 64
+        assert flavour.heap_address_bits == 40
+        assert flavour.bug_compat == 2
 
     def test_equality(self):
         flavour1 = spead2.Flavour(4, 64, 40, 2)
         flavour1b = spead2.Flavour(4, 64, 40, 2)
         flavour2 = spead2.Flavour(4, 64, 48, 2)
         flavour3 = spead2.Flavour(4, 64, 40, 4)
-        assert_true(flavour1 == flavour1b)
-        assert_true(flavour1 != flavour2)
-        assert_false(flavour1 != flavour1b)
-        assert_false(flavour1 == flavour2)
-        assert_false(flavour1 == flavour3)
+        assert flavour1 == flavour1b
+        assert flavour1 != flavour2
+        assert not (flavour1 != flavour1b)
+        assert not (flavour1 == flavour2)
+        assert not (flavour1 == flavour3)
 
 
 class TestItem:
@@ -105,59 +102,62 @@ class TestItem:
                             (None,), format=[('c', 8)], value='\u0200')
         item2 = spead2.Item(0x1001, 'name2', 'description2', (),
                             dtype='S5', value='\u0201')
-        assert_raises(UnicodeEncodeError, item1.to_buffer)
-        assert_raises(UnicodeEncodeError, item2.to_buffer)
+        with pytest.raises(UnicodeEncodeError):
+            item1.to_buffer()
+        with pytest.raises(UnicodeEncodeError):
+            item2.to_buffer()
 
     def test_format_and_dtype(self):
         """Specifying both a format and dtype raises :py:exc:`ValueError`."""
-        assert_raises(ValueError, spead2.Item, 0x1000, 'name', 'description',
-                      (1, 2), format=[('c', 8)], dtype='S1')
+        with pytest.raises(ValueError):
+            spead2.Item(0x1000, 'name', 'description',
+                        (1, 2), format=[('c', 8)], dtype='S1')
 
     def test_no_format_or_dtype(self):
         """At least one of format and dtype must be specified."""
-        assert_raises(ValueError, spead2.Item, 0x1000, 'name', 'description',
-                      (1, 2), format=None)
+        with pytest.raises(ValueError):
+            spead2.Item(0x1000, 'name', 'description', (1, 2), format=None)
 
     def test_invalid_order(self):
         """The `order` parameter must be either 'C' or 'F'."""
-        assert_raises(ValueError, spead2.Item, 0x1000, 'name', 'description',
-                      (1, 2), np.int32, order='K')
+        with pytest.raises(ValueError):
+            spead2.Item(0x1000, 'name', 'description', (1, 2), np.int32, order='K')
 
     def test_fortran_fallback(self):
-        """The `order` parameter must be either 'C' for legacy formats."""
-        assert_raises(ValueError, spead2.Item, 0x1000, 'name', 'description',
-                      (1, 2), format=[('u', 32)], order='F')
+        """The `order` parameter must be 'C' for legacy formats."""
+        with pytest.raises(ValueError):
+            spead2.Item(0x1000, 'name', 'description', (1, 2), format=[('u', 32)], order='F')
 
     def test_empty_format(self):
         """Format must not be empty"""
-        assert_raises(ValueError, spead2.Item, 0x1000, 'name', 'description',
-                      (1, 2), format=[])
+        with pytest.raises(ValueError):
+            spead2.Item(0x1000, 'name', 'description', (1, 2), format=[])
 
     def test_assign_none(self):
         """Changing a value back to `None` raises :py:exc:`ValueError`."""
         item = spead2.Item(0x1000, 'name', 'description', (), np.int32)
-        with assert_raises(ValueError):
+        with pytest.raises(ValueError):
             item.value = None
 
     def test_multiple_unknown(self):
         """Multiple unknown dimensions are not allowed."""
-        assert_raises(ValueError, spead2.Item, 0x1000, 'name', 'description',
-                      (5, None, 3, None), format=[('u', 32)])
+        with pytest.raises(ValueError):
+            spead2.Item(0x1000, 'name', 'description', (5, None, 3, None), format=[('u', 32)])
 
     def test_numpy_unknown(self):
         """Unknown dimensions are not permitted when using a numpy descriptor"""
-        assert_raises(ValueError, spead2.Item, 0x1000, 'name', 'description',
-                      (5, None), np.int32)
+        with pytest.raises(ValueError):
+            spead2.Item(0x1000, 'name', 'description', (5, None), np.int32)
 
     def test_nonascii_name(self):
         """Name with non-ASCII characters must fail"""
-        with assert_raises(UnicodeEncodeError):
+        with pytest.raises(UnicodeEncodeError):
             item = spead2.Item(0x1000, '\u0200', 'description', (), np.int32)
             item.to_raw(spead2.Flavour())
 
     def test_nonascii_description(self):
         """Description with non-ASCII characters must fail"""
-        with assert_raises(UnicodeEncodeError):
+        with pytest.raises(UnicodeEncodeError):
             item = spead2.Item(0x1000, 'name', '\u0200', (), np.int32)
             item.to_raw(spead2.Flavour())
 
@@ -173,9 +173,9 @@ class TestItemGroup:
         ig.add_item(None, 'item 3', 'item 3', (), np.int32)
         ig.add_item(None, 'item 4', 'item 4', (), np.int32)
         ig.add_item(None, 'item 5', 'item 5', (), np.int32)
-        assert_equal('item 3', ig[0x1001].name)
-        assert_equal('item 4', ig[0x1002].name)
-        assert_equal('item 5', ig[0x1004].name)
+        assert ig[0x1001].name == 'item 3'
+        assert ig[0x1002].name == 'item 4'
+        assert ig[0x1004].name == 'item 5'
 
     def test_replace_rename(self):
         """When a new item is added with a known ID but a different name, the
@@ -183,8 +183,8 @@ class TestItemGroup:
         ig = spead2.ItemGroup()
         ig.add_item(0x1000, 'item 1', 'item 1', (), np.int32)
         ig.add_item(0x1000, 'renamed', 'renamed', (), np.int32)
-        assert_equal([0x1000], list(ig.ids()))
-        assert_equal(['renamed'], list(ig.keys()))
+        assert list(ig.ids()) == [0x1000]
+        assert list(ig.keys()) == ['renamed']
 
     def test_replace_clobber_name(self):
         """When a new item is added that collides with an existing name, that
@@ -192,10 +192,10 @@ class TestItemGroup:
         ig = spead2.ItemGroup()
         ig.add_item(0x1000, 'item 1', 'item 1', (), np.int32)
         ig.add_item(0x1001, 'item 1', 'clobbered', (), np.int32)
-        assert_equal([0x1001], list(ig.ids()))
-        assert_equal(['item 1'], list(ig.keys()))
-        assert_equal('clobbered', ig['item 1'].description)
-        assert_is(ig['item 1'], ig[0x1001])
+        assert list(ig.ids()) == [0x1001]
+        assert list(ig.keys()) == ['item 1']
+        assert ig['item 1'].description == 'clobbered'
+        assert ig['item 1'] is ig[0x1001]
 
     def test_replace_reset_value(self):
         """When a descriptor is replaced with an identical one but a new
@@ -210,12 +210,12 @@ class TestItemGroup:
         item2_version = item2.version
         ig.add_item(0x1000, 'item 1', 'item 1', (), np.int32, value=np.int32(6))
         ig.add_item(0x1001, 'item 2', 'item 2', (), np.int32)
-        assert_is(item1, ig[0x1000])
-        assert_is(item2, ig[0x1001])
-        assert_equal(np.int32(6), item1.value)
-        assert_greater(item1.version, item1_version)
-        assert_equal(np.int32(5), item2.value)
-        assert_equal(item2.version, item2_version)
+        assert item1 is ig[0x1000]
+        assert item2 is ig[0x1001]
+        assert item1.value == np.int32(6)
+        assert item1.version > item1_version
+        assert item2.value == np.int32(5)
+        assert item2_version == item2.version
 
     def test_replace_change_shape(self):
         """When a descriptor changes the shape, the old item must be discarded
@@ -225,9 +225,9 @@ class TestItemGroup:
         item1 = ig[0x1000]
         item1_version = item1.version
         ig.add_item(0x1000, 'item 1', 'bigger', (3, 4), np.int32)
-        assert_is_not(item1, ig[0x1000])
-        assert_is_none(ig[0x1000].value)
-        assert_greater(ig[0x1000].version, item1_version)
+        assert item1 is not ig[0x1000]
+        assert ig[0x1000].value is None
+        assert ig[0x1000].version > item1_version
 
     def test_replace_clobber_both(self):
         """Adding a new item that collides with one item on name and another on
@@ -236,7 +236,7 @@ class TestItemGroup:
         ig.add_item(0x1000, 'item 1', 'item 1', (), np.int32)
         ig.add_item(0x1001, 'item 2', 'item 2', (), np.int32)
         ig.add_item(0x1000, 'item 2', 'clobber', (), np.int32)
-        assert_equal([0x1000], list(ig.ids()))
-        assert_equal(['item 2'], list(ig.keys()))
-        assert_is(ig[0x1000], ig['item 2'])
-        assert_equal('clobber', ig[0x1000].description)
+        assert list(ig.ids()) == [0x1000]
+        assert list(ig.keys()) == ['item 2']
+        assert ig[0x1000] is ig['item 2']
+        assert ig[0x1000].description == 'clobber'
