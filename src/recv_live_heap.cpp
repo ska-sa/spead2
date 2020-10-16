27742487342b7560d66a937bfb007cd37a9bec0a
Bruce Merry <bmerry@ska.ac.za>
2020-10-16 15:34:51 +0200

    Update copyright years and add missing notices

--- src/recv_live_heap.cpp
+++ src/recv_live_heap.cpp
@@ -1,4 +1,4 @@
-/* Copyright 2015 National Research Foundation (SARAO)
+/* Copyright 2015, 2020 National Research Foundation (SARAO)
  *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
