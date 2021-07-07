6a4a5f1efc97b677bf9d63c805c53492c3c72438
Bruce Merry <bmerry@ska.ac.za>
2021-07-07 11:50:00 +0200

    Use setuptools_scm and pypa-build for building

--- manylinux/generate_wheels.sh
+++ manylinux/generate_wheels.sh
@@ -6,12 +6,12 @@ mkdir -p /output
 version="$(sed 's/.*"\(.*\)"/\1/' src/spead2/_version.py)"
 for d in /opt/python/cp{36,37,38,39}*; do
     git clean -xdf
-    $d/bin/pip install jinja2==2.10.1 pycparser==2.19   # For bootstrap
+    $d/bin/pip install jinja2==3.0.1 pycparser==2.20 build==0.5.1   # For bootstrap and build
     PATH=$d/bin:$PATH ./bootstrap.sh
     echo "[build_ext]" > setup.cfg
     echo "split_debug = /output" >> setup.cfg
-    $d/bin/pip wheel --no-deps -v .
-    auditwheel repair --plat manylinux2014_x86_64 -w /output spead2-*-`basename $d`-linux_*.whl
+    $d/bin/python -m build
+    auditwheel repair --plat manylinux2014_x86_64 -w /output dist/spead2-*-`basename $d`-linux_*.whl
 done
 cd /output
 tar -Jcvf spead2-$version-debug.tar.xz _spead2*.debug
