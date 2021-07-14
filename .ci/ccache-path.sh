fbc253123c12a4c9b164e66635f06310b392e3fa
Bruce Merry <bmerry@ska.ac.za>
2021-07-14 15:17:29 +0200

    Small tweaks to CI
    - Make coverage ccache cache use all files to compute hash, to prevent
      compiler options changes from using a stale cache.
    - Add some comments to scripts in .ci

--- .ci/ccache-path.sh
+++ .ci/ccache-path.sh
@@ -5,4 +5,5 @@ for path in /usr/lib/ccache /usr/local/opt/ccache/libexec; do
         echo "$path" >> $GITHUB_PATH
     fi
 done
+# Clear the statistics so that we can later get statistics for just this run.
 ccache -z
--- .ci/py-tests.sh
+++ .ci/py-tests.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -e
 
+# -rs summarises the reasons for skipping tests
 pytest -v -rs
 for test in test_logging_shutdown test_running_thread_pool test_running_stream; do
     echo "Running shutdown test $test"
