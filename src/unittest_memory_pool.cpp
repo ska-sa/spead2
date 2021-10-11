c91701ed9097e3d67c64c652fa18cb6eb15c9c04
Bruce Merry <bmerry@ska.ac.za>
2021-10-11 15:47:46 +0200

    Add 2021 to a copyright notice

--- src/unittest_memory_pool.cpp
+++ src/unittest_memory_pool.cpp
@@ -1,4 +1,4 @@
-/* Copyright 2016, 2017, 2019 National Research Foundation (SARAO)
+/* Copyright 2016, 2017, 2019, 2021 National Research Foundation (SARAO)
  *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
