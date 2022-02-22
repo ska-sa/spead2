fbdc282b514a6eeaca1fc060b48dd5d3c7b2f89a
Bruce Merry <bmerry@sarao.ac.za>
2022-02-22 10:10:30 +0200

    Update versions used of some things
    Mainly *downgrading* setuptools to work around a bug, but upgrading
    pybind11, jinja2, pycparser, cibuildwheel.

--- manylinux/before_build.sh
+++ manylinux/before_build.sh
@@ -3,5 +3,7 @@
 
 set -e -u
 
-pip install jinja2==3.0.1 pycparser==2.20
+# setuptools is pinned to an older version to work around
+# https://github.com/pypa/setuptools/issues/3130
+pip install jinja2==3.0.3 pycparser==2.21 setuptools==59.8.0
 ./bootstrap.sh
