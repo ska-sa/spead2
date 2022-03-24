0dacb05769ae9233f76007dff0879810c33a95e9
Bruce Merry <bmerry@sarao.ac.za>
2022-03-24 13:20:57 +0200

    Another attempt to install libdivide on MacOS

--- .ci/install-sys-pkgs.sh
+++ .ci/install-sys-pkgs.sh
@@ -17,5 +17,6 @@ if [ "$(uname -s)" = "Linux" ]; then
         libibverbs-dev \
         libdivide-dev
 else
-    brew install autoconf automake boost ccache libdivide
+    brew install autoconf automake boost ccache
+    wget https://raw.githubusercontent.com/ridiculousfish/libdivide/5.0/libdivide.h -O /usr/local/include/libdivide.h
 fi
