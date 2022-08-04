78c19668ec09f7afff7959bd9377a6714267eaf6
Bruce Merry <bmerry@sarao.ac.za>
2022-08-04 14:27:17 +0200

    Reduce parallelism in CI builds
    I'm hoping this will prevent the
    ```
    g++: fatal error: Killed signal terminated program cc1plus
    ```
    
    errors I've been seeing.

--- .ci/cxx-tests.sh
+++ .ci/cxx-tests.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 set -e
 
-if ! make -C build -j check; then
+# -j4 rather than -j to avoid getting killed on Github Actions
+if ! make -C build -j4 check; then
   cat build/src/test-suite.log
   exit 1
 fi
