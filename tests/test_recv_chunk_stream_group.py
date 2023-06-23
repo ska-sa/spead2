3a9fcbcd9f285aa15ce16ed77b97d656a2b1d842
Bruce Merry <bmerry@sarao.ac.za>
2023-06-23 13:25:17 +0200

    Add chunk_stream_group test where one stream does nothing

--- tests/test_recv_chunk_stream_group.py
+++ tests/test_recv_chunk_stream_group.py
@@ -99,39 +99,40 @@ class TestChunkStreamRingGroup:
     def send_stream(self, queues):
         return send.InprocStream(spead2.ThreadPool(), queues, send.StreamConfig())
 
-    @pytest.fixture
-    def item_group(self):
+    def _send_data(self, send_stream, data, heaps=None):
+        """Send the data.
+
+        To send only a subset of heaps (or to send out of order), pass the
+        indices to skip in `heaps`.
+        """
+        data_by_heap = data.reshape(-1, HEAP_PAYLOAD_SIZE)
         ig = spead2.send.ItemGroup()
         ig.add_item(0x1000, 'position', 'position in stream', (), format=[('u', 32)])
         ig.add_item(0x1001, 'payload', 'payload data', (HEAP_PAYLOAD_SIZE,), dtype=np.uint8)
-        return ig
-
-    def test_full_in_order(self, group, queues, recv_streams, send_stream, data_ring, free_ring, item_group):
-        chunks = 20
-        rng = np.random.default_rng(seed=1)
-        data = rng.integers(0, 256, chunks * CHUNK_PAYLOAD_SIZE, np.uint8)
-        data_by_chunk = data.reshape(chunks, -1)
-        data_by_heap = data.reshape(chunks * HEAPS_PER_CHUNK, -1)
-
         # Stream groups are impractical to test deterministically, because
         # they rely on concurrent forward progress. So we just feed the
         # data in slowly enough that we expect heaps provided before a
         # sleep to be processed before those after the sleep.
-        def send_data():
-            for i, payload in enumerate(data_by_heap):
-                if i % STREAMS == 0:
-                    time.sleep(0.005)
-                item_group['position'].value = i
-                item_group['payload'].value = payload
-                heap = item_group.get_heap(data='all', descriptors='none')
-                send_stream.send_heap(heap, substream_index=i % STREAMS)
-            # Stop all the queues, which should flush everything and stop the data
-            # ring.
-            time.sleep(0.01)
-            for queue in queues:
-                queue.stop()
+        if heaps is None:
+            heaps = range(len(data_by_heap))
+        for i in heaps:
+            ig['position'].value = i
+            ig['payload'].value = data_by_heap[i]
+            heap = ig.get_heap(data='all', descriptors='none')
+            send_stream.send_heap(heap, substream_index=i % STREAMS)
+            time.sleep(0.001)
+        # Stop all the queues, which should flush everything and stop the
+        # data ring.
+        for queue in send_stream.queues:
+            queue.stop()
 
-        send_thread = threading.Thread(target=send_data)
+    def test_full_in_order(self, group, queues, recv_streams, send_stream, data_ring, free_ring):
+        """Send all the data, in order."""
+        chunks = 20
+        rng = np.random.default_rng(seed=1)
+        data = rng.integers(0, 256, chunks * CHUNK_PAYLOAD_SIZE, np.uint8)
+        data_by_chunk = data.reshape(chunks, -1)
+        send_thread = threading.Thread(target=self._send_data, args=(send_stream, data))
         send_thread.start()
 
         for i in range(chunks):
@@ -139,6 +140,36 @@ class TestChunkStreamRingGroup:
             assert chunk.chunk_id == i
             np.testing.assert_equal(chunk.present, 1)
             np.testing.assert_equal(chunk.data, data_by_chunk[i])
+            group.add_free_chunk(chunk)
+
+        # Stopping all the queues should shut down the data ringbuffer
+        with pytest.raises(spead2.Stopped):
+            data_ring.get()
+
+        send_thread.join()
+
+    def test_missing_stream(self, group, queues, recv_streams, send_stream, data_ring, free_ring):
+        """Skip sending data to one of the streams."""
+        chunks = 20
+        rng = np.random.default_rng(seed=1)
+        data = rng.integers(0, 256, chunks * CHUNK_PAYLOAD_SIZE, np.uint8)
+        data_by_heap = data.reshape(chunks, HEAPS_PER_CHUNK, -1)
+        heaps = [i for i in range(chunks * HEAPS_PER_CHUNK) if i % STREAMS != 2]
+        send_thread = threading.Thread(target=self._send_data, args=(send_stream, data, heaps))
+        send_thread.start()
+
+        expected_present = np.ones(chunks * HEAPS_PER_CHUNK, bool)
+        expected_present[2::STREAMS] = False
+        expected_present = expected_present.reshape(chunks, HEAPS_PER_CHUNK)
+
+        for i in range(chunks):
+            chunk = data_ring.get()
+            assert chunk.chunk_id == i
+            np.testing.assert_equal(chunk.present, expected_present[i])
+            actual_data = chunk.data.reshape(HEAPS_PER_CHUNK, -1)
+            for j in range(HEAPS_PER_CHUNK):
+                if expected_present[i, j]:
+                    np.testing.assert_equal(actual_data[j], data_by_heap[i, j])
             group.add_free_chunk(chunk)
 
         # Stopping all the queues should shut down the data ringbuffer
