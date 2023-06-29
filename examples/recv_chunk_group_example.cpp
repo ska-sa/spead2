8405e452906d406abe6c1692f29542f342b7b84f
Bruce Merry <bmerry@sarao.ac.za>
2023-06-29 17:30:19 +0200

    Add example C++ program using chunk_stream_ring_group

--- examples/recv_chunk_group_example.cpp
+++ examples/recv_chunk_group_example.cpp
@@ -1,4 +1,4 @@
-/* Copyright 2021 National Research Foundation (SARAO)
+/* Copyright 2023 National Research Foundation (SARAO)
  *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
@@ -17,9 +17,9 @@
 /**
  * @file
  *
- * This is an example of using the chunking receive API with ringbuffers. To
- * test it, run
- * <code>spead2_send localhost:8888 --heaps 1000 --heap-size 65536 --rate 10</code>.
+ * This is an example of using the chunk stream group API with ringbuffers.
+ * To test it, run
+ * <code>spead2_send localhost:8888 localhost:8889 --heaps 1000 --heap-size 65536 --rate 10</code>.
  */
 
 #include <iostream>
@@ -33,7 +33,7 @@
 #include <spead2/common_memory_allocator.h>
 #include <spead2/common_ringbuffer.h>
 #include <spead2/recv_stream.h>
-#include <spead2/recv_chunk_stream.h>
+#include <spead2/recv_chunk_stream_group.h>
 #include <spead2/recv_udp.h>
 
 static constexpr std::size_t heap_payload_size = 65536;
@@ -56,31 +56,41 @@ static void chunk_place(spead2::recv::chunk_place_data *data, std::size_t data_s
 
 int main()
 {
+    constexpr std::size_t num_streams = 2;
     constexpr int max_chunks = 4;
     auto chunk_config = spead2::recv::chunk_stream_config()
         .set_items({spead2::HEAP_CNT_ID, spead2::HEAP_LENGTH_ID})
         .set_max_chunks(max_chunks)
         .set_place(chunk_place);
     auto stream_config = spead2::recv::stream_config();
+    auto group_config = spead2::recv::chunk_stream_group_config()
+        .set_max_chunks(max_chunks);
     using chunk_ringbuffer = spead2::ringbuffer<std::unique_ptr<spead2::recv::chunk>>;
     auto data_ring = std::make_shared<chunk_ringbuffer>(max_chunks);
     auto free_ring = std::make_shared<chunk_ringbuffer>(max_chunks);
     auto allocator = std::make_shared<spead2::memory_allocator>();
 
-    spead2::thread_pool worker;
-    spead2::recv::chunk_ring_stream<> stream(worker, stream_config, chunk_config, data_ring, free_ring);
+    spead2::recv::chunk_stream_ring_group<> group(group_config, data_ring, free_ring);
+    spead2::thread_pool workers[num_streams];
+    for (std::size_t i = 0; i < num_streams; i++)
+    {
+        group.emplace_back(workers[i], stream_config, chunk_config);
+    }
     for (int i = 0; i < max_chunks; i++)
     {
         std::unique_ptr<spead2::recv::chunk> chunk{new spead2::recv::chunk};
         chunk->present = allocator->allocate(heaps_per_chunk, nullptr);
         chunk->present_size = heaps_per_chunk;
         chunk->data = allocator->allocate(chunk_payload_size, nullptr);
-        stream.add_free_chunk(std::move(chunk));
+        group.add_free_chunk(std::move(chunk));
     }
 
-    boost::asio::ip::udp::endpoint endpoint(boost::asio::ip::address_v4::any(), 8888);
-    stream.emplace_reader<spead2::recv::udp_reader>(
-        endpoint, spead2::recv::udp_reader::default_max_size, 1024 * 1024);
+    for (std::size_t i = 0; i < num_streams; i++)
+    {
+        boost::asio::ip::udp::endpoint endpoint(boost::asio::ip::address_v4::any(), 8888 + i);
+        group[i].emplace_reader<spead2::recv::udp_reader>(
+            endpoint, spead2::recv::udp_reader::default_max_size, 1024 * 1024);
+    }
     while (true)
     {
         try
@@ -91,7 +101,7 @@ int main()
                 chunk->present.get() + chunk->present_size, std::size_t(0));
             std::cout << "Received chunk " << chunk->chunk_id << " with "
                 << n_present << " / " << heaps_per_chunk << " heaps\n";
-            stream.add_free_chunk(std::move(chunk));
+            group.add_free_chunk(std::move(chunk));
         }
         catch (spead2::ringbuffer_stopped &)
         {
