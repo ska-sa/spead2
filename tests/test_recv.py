76672d4622f2fb05b6da089e454d33a7db3114cb
Bruce Merry <bmerry@sarao.ac.za>
2023-09-04 09:27:00 +0200

    Use a more generic workaround for pytest-dev/pytest#11374
    Instead of selectively clearing members in teardown methods, just clear
    the whole object every time using a pytest hook.

--- tests/test_recv.py
+++ tests/test_recv.py
@@ -1246,8 +1246,6 @@ class TestTcpReader:
 
     def teardown_method(self):
         self.close()
-        # Workaround for https://github.com/pytest-dev/pytest/issues/11374
-        self.receiver = None
 
     def close(self):
         if self.send_sock is not None:
