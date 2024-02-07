a546ab39ebdf9f998a2a293869f437573b92cc75
Bruce Merry <bmerry@sarao.ac.za>
2024-02-07 16:10:09 +0200

    Update copyright notices

--- src/py_send.cpp
+++ src/py_send.cpp
@@ -1,4 +1,4 @@
-/* Copyright 2015, 2017, 2019-2021, 2023 National Research Foundation (SARAO)
+/* Copyright 2015, 2017, 2019-2021, 2023-2024 National Research Foundation (SARAO)
  *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
