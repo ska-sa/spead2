c44fdf24a7fc675f9627fc37f5768e40122d141f
Bruce Merry <bmerry@sarao.ac.za>
2024-05-20 16:03:31 +0200

    CI: Set SPEAD2_TEST_IBV_INTERFACE_ADDRESS
    if given by a command-line option to py-tests.sh. This will support
    testing on Jenkins.

--- .ci/py-tests.sh
+++ .ci/py-tests.sh
@@ -1,5 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 set -e
+
+if [ "$1" = "--ibverbs" ]; then
+    # The force_local_lb_disable file is specific to mlx5. We also want to
+    # ensure we pick a device where it is off, since we depend on the loopback
+    # for the test.
+    device=$(grep -l 'Force local loopback disable is OFF' /sys/class/net/*/settings/force_local_lb_disable | cut -d/ -f5 | head -n1)
+    if [ -z "$device" ]; then
+        echo "No ibverbs device with local multicast loopback found" 1>&2
+        exit 1
+    fi
+    # Get IPv4 address
+    address=$(ip -j addr list dev "$device" | jq -r '.[0].addr_info[] | select(.family == "inet") | .local')
+    if [ -z "$address" ]; then
+        echo "No IPv4 address found for device $device" 1>&2
+        exit 1
+    fi
+    export SPEAD2_TEST_IBV_INTERFACE_ADDRESS="$address"
+    echo "Testing with device $device address $address"
+fi
 
 # -ra summarises the reasons for skipping or failing tests
 pytest -v -ra
