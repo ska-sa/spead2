288bf7541a32ed908762d1faf3268ee2322689d2
Bruce Merry <bmerry@sarao.ac.za>
2025-09-29 14:06:57 +0200

    Switch from pip-tools to uv
    This allows creating a single universal requirements.txt spanning all
    Python versions, which simplified the need to have separate requirements
    files for the oldest and newest versions.
    
    Also remove most requirements from requirements.in in favour of
    extracting them from pyproject.toml.

--- .ci/py-requirements.sh
+++ .ci/py-requirements.sh
@@ -2,8 +2,4 @@
 set -e -u
 
 pip install -U pip
-if [ "$(python -c 'import sys; print(sys.version_info >= (3, 13))')" == "True" ]; then
-    pip install -r requirements-3.13.txt
-else
-    pip install -r requirements.txt
-fi
+pip install -r requirements.txt
